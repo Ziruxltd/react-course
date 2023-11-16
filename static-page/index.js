19c09ade6492d4e7148817cbb413dfa75b97ab63
Antonio <antoniojaramillofanta@gmail.com>
2023-11-16 15:21:19 +0100

    render

--- static-page/index.js
+++ static-page/index.js
@@ -1,4 +1,12 @@
-const h1 = document.createElement("h1")
-h1.textContent = "This is an imperative way to program"
-h1.className = "header"
-document.getElementById("root").append(h1)
+const navbar = (
+    <nav>
+        <h1>Bob's Bistro</h1>
+        <ul>
+            <li>Menu</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(navbar, document.getElementById("root"))
