0f5514b52700cdc592ccf806e5ed2621ef5e7e64
Antonio <antoniojaramillofanta@gmail.com>
2023-11-17 09:55:06 +0100

    added parent

--- static-page/index.js
+++ static-page/index.js
@@ -1,12 +1,32 @@
-const navbar = (
-    <nav>
-        <h1>Bob's Bistro</h1>
-        <ul>
-            <li>Menu</li>
-            <li>About</li>
-            <li>Contact</li>
-        </ul>
-    </nav>
-)
+const { React, ReactDOM, createRoot } = window;
 
-ReactDOM.render(navbar, document.getElementById("root"))
+function Header() {
+    return (
+        <header>
+            <nav>
+                <img src="./react-logo.png" width="40px" />
+            </nav>
+        </header>
+    )
+}
+
+function Page() {
+    return (
+        <div>
+            <Header />
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It's a popular library, so I'll be 
+                able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                if I know React</li>
+            </ol>
+            <footer>
+                <small>© 2021 Ziroll development. All rights reserved.</small>
+            </footer>
+        </div>
+    )
+}
+
+const domNode = document.getElementById('navigation');
+ReactDOM.render(<Page />, domNode);
